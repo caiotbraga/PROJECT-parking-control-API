98493310d544ad37299f171d4341b754174732f4
Caio Braga <caiotabraga@hotmail.com>
2023-04-26 08:50:21 -0300

    save implemented

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -1,7 +1,9 @@
 package com.api.parkingcontrol.services;
 
+import com.api.parkingcontrol.models.ParkingSpotModel;
 import com.api.parkingcontrol.repositories.ParkingSpotRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ParkingSpotService {
@@ -9,6 +11,11 @@ public class ParkingSpotService {
     final ParkingSpotRepository parkingSpotRepository;
     public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
         this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel){
+        return parkingSpotRepository.save(parkingSpotModel);
     }
 }
 
